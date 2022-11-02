03ca23ba9d1279829003972a8b5de3670227d42d
unknown <nashithavani07@gmail.com>
2022-11-02 20:42:31 +0530

    code fixed

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let str = "Madam";
+let bag = ""
+  for(let i=str.length-1;i>=0;i--){
+      
+          bag=bag+str[i];
+      
+  }
+  if(bag==str){
+      console.log("Yes");
+  } else {
+      console.log("No");
+  }
